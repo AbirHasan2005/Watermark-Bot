821dbd1a66723994d405ddb64379b5c0f31b24db
Abir Hasan <hackitabirhasan@gmail.com>
2022-05-12 20:15:28 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ async def SettingsBot(bot, cmd):
 	)
 
 
-@AHBot.on_message(filters.document | filters.video | filters.photo & filters.private)
+@AHBot.on_message((filters.document | filters.video | filters.photo) & filters.private)
 async def VidWatermarkAdder(bot, cmd):
 	if not await db.is_user_exist(cmd.from_user.id):
 		await db.add_user(cmd.from_user.id)
